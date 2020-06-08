5f6ebd6d8913bbd3cd0e9baa99b4264bb0345094
Pedro Vitor <pedrocavt@gmail.com>
2020-06-08 19:30:44 -0300

    Mexendo no Session e no LocalStorage

--- js/User.js
+++ js/User.js
@@ -1,5 +1,8 @@
 class User {
 
+    KEY_USER_LOGGED = "user_logged" //Variavel para chamar na SessionStorage
+    KEY_USER_LOGGED_EMAIL = "user_logged_email" //Variavel para chamar na SessionStorage
+
     usersAllwoed = [
 
         {
@@ -13,20 +16,50 @@ class User {
         }
     ]
 
-    login(email, password) {
-        this.usersAllwoed.map(function(item, index) {
-            if (item.email == email) {
+    login(email, password, rememberMe) {
+        var result = false
 
+        var self = this
+
+        this.usersAllwoed.map(function(item, index) {
+
+
+            if (item.email == email) {
                 if (item.password == password) {
-                    alert("Usário tem permissão para acessar")
-                } else {
-                    alert("Seu email esta certo, mas sua senha esta errada")
+                    result = true
+
+                    if (rememberMe == true) {
+                        localStorage.setItem(self.KEY_USER_LOGGED, true) //registrar no SessionStorage que o usuario esta logado
+                        localStorage.setItem(self.KEY_USER_LOGGED_EMAIL, email) //registrar no SessionStorage o email do usuário
+
+                    } else {
+                        sessionStorage.setItem(self.KEY_USER_LOGGED, true) //registrar no SessionStorage que o usuario esta logado
+                        sessionStorage.setItem(self.KEY_USER_LOGGED_EMAIL, email) //registrar no SessionStorage o email do usuário
+                    }
+
+
                 }
             }
         })
+        return result
+
     }
 
     logout() {
 
+    }
+
+    isAuthenticated() {
+
+        var sessionStorageLogged = sessionStorage.getItem(this.KEY_USER_LOGGED)
+        var localStorageLogged = localStorage.getItem(this.KEY_USER_LOGGED)
+
+        if (localStorageLogged == "true") {
+            return true
+        }
+
+        if (sessionStorageLogged == "true") {
+            return true
+        }
     }
 }
--- js/main.js
+++ js/main.js
@@ -1,12 +1,36 @@
-let myUser = new User()
+var myUser = new User()
+
+function showTodoListContent() {
+    $("#loginContent").hide("slow")
+    $("#todoListContent").show("slow")
+}
+
 
 $(document).ready(function() {
+
+    var resultIsAuthenticated = myUser.isAuthenticated()
+    if (resultIsAuthenticated) {
+        showTodoListContent()
+    } else {
+        $("#loginContent").show('slow')
+    }
+
 
     $("#buttonSignIn").click(function() {
 
         let inputEmail = $("#inputEmail").val()
         let inputPassword = $("#inputPassword").val()
 
-        myUser.login(inputEmail, inputPassword)
+        var rememberMe = $("#rememberMe").is(":checked")
+
+        var resultLogin = myUser.login(inputEmail, inputPassword, rememberMe)
+
+        if (resultLogin == true) {
+            alert("Usuário logado com sucesso")
+            showTodoListContent()
+
+        } else {
+            alert("Verifique seus dados e tente novamente")
+        }
     })
 })
